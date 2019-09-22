d937c625c95d0dade2ed05f92577087797464c19
CBraeuninger <carolin.braeuninger@gmail.com>
2019-09-22 19:56:03 +0200

    Camera calibration function completed

--- camera_calibration/_init_.py
+++ camera_calibration/_init_.py
@@ -5,5 +5,10 @@ Created on 16 sept. 2019
 '''
 from camera_calibration import calibrate_camera
 
-calibrate_camera()
+mtx, dist = calibrate_camera(False)
+
+print("camera matrix:")
+print(mtx)
+print("distortion coefficients:")
+print(dist)
     
